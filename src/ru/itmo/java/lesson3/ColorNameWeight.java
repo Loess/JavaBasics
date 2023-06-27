7502a837eb177a78e51b3ef3ff4b491b9bbc5ec1
BlacK <smartwiz@mail.ru>
2023-06-27 20:46:31 +0300

    lesson3

---
+++ src/ru/itmo/java/lesson3/ColorNameWeight.java
@@ -0,0 +1,43 @@
+package ru.itmo.java.lesson3;
+
+public class ColorNameWeight {
+    public String color;
+    private String name;
+    private int weight;
+
+    public ColorNameWeight(String color, String name, int weight) {
+        this.color = color;
+        this.name = name;
+        this.weight = weight;
+    }
+    public ColorNameWeight(String color) {
+        this.color = color;
+    }
+    public ColorNameWeight(String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+    public ColorNameWeight() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "ColorNameWeight{" +
+                "color='" + color + '\'' +
+                ", name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
+
+class JavaProgram2 {
+    public static void main(String[] args) {
+        ColorNameWeight obj1 = new ColorNameWeight("funny","object1",10);
+        ColorNameWeight obj2 = new ColorNameWeight("white",10);
+        System.out.println(obj1);
+        obj1.color = "not funny";
+        System.out.println(obj1);
+        System.out.println(obj2);
+    }
+}
