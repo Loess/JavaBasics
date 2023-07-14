06af2439a520d6769b7d004de9599233d20b51a1
BlacK <smartwiz@mail.ru>
2023-07-14 21:33:52 +0300

    lesson9

---
+++ src/ru/itmo/java/lesson9/User.java
@@ -0,0 +1,17 @@
+package ru.itmo.java.lesson9;
+
+public class User {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+}
